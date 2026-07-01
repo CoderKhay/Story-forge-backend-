0e8abbf99422bbd4adca38ee4334fb15652a051e
Muhammad Kamaldeen <kamaldeenomotolani@gmail.com>
2026-07-01 22:24:56 +0100

    changed the environmental variable names in my pool config

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ import pg from "pg";
 const { Pool } = pg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGNAME,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT),
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
