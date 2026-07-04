bafe3f246926edaf94ec12e65fc791ad8cc74c0d
Muhammad Kamaldeen <kamaldeenomotolani@gmail.com>
2026-07-04 11:59:32 +0100

    reverted the pool connection variables to how they were

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ import pg from "pg";
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT),
+  //  connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
