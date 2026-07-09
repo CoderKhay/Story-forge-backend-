441678646d7cc0770160e5dc06a3a69ec39438c8
Muhammad Kamaldeen <kamaldeenomotolani@gmail.com>
2026-07-09 14:33:03 +0100

    limited access to the backend url

--- src/storyForge.js
+++ src/storyForge.js
@@ -4,7 +4,14 @@ import { greeting, getDB } from "./controllers/controllers.js";
 
 export const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://forged-story.netlify.app"
+        : true,
+  }),
+);
 
 app.get("/", greeting);
 app.get("/story", getDB);
