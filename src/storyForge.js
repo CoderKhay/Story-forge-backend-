a8d211a9e56f6b7f8320e0e353afc077a16279a2
Muhammad Kamaldeen <kamaldeenomotolani@gmail.com>
2026-07-17 17:13:56 +0100

    removed the trailing slash, it is a breaking chang

--- src/storyForge.js
+++ src/storyForge.js
@@ -8,7 +8,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? "https://forged-story.netlify.app/"
+        ? "https://forged-story.netlify.app"
         : true,
   }),
 );
